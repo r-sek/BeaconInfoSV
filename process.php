656ce9cc292f523a565452991d0d4ba2094d9e66
kurokirishima9622 <rs960222@gmail.com>
2017-02-12 19:41:34 +0900

    ファイル名変更

--- process.php
+++ process.php
@@ -7,4 +7,12 @@
  */
 $link = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
 
+$query="SELECT * FROM ".beaconinfodb.spot_master."'";
+$res=mysql_query($query);
+$output=array();
+while($e=mysql_fetch_assoc($res)){$output[]=$e;}
+mysql_free_result($res);
+mysql_close($con);
+echo json_encode($output);
+
 ?>
