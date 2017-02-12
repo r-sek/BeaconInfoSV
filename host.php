a868d183f23fff95eb83e6f8399700a40039233f
kurokirishima9622 <rs960222@gmail.com>
2017-02-12 18:56:42 +0900

    FIrstCommit

---
+++ host.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rj
+ * Date: 2017/02/12
+ * Time: 17:55
+ */
+$link = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+
+?>
