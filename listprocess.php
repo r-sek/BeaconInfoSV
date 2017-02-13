b156c1c1ade8b5df89112f2883720cb1e2a81891
redspirit-rj <rs960222@gmail.com>
2017-02-13 18:33:56 +0900

    頑張る

--- listprocess.php
+++ listprocess.php
@@ -15,7 +15,7 @@ $mysqli = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVE
 $lang = $_GET['lang']; //言語情報の取得(jp or en)
 $sort = $_GET['sort']; //ソート条件の格納
 
-$sql = "SELECT * FROM beaconinfodb.spot_master";
+$sql = "SELECT spotmaster.spotid,spot_ja.spot_name FROM beaconinfodb.spot_master,beaconinfodb.spot_ja";
 
 
 ?>
