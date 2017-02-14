a66e101d3e579761e8bb46ddd7811270133324cf
kurokirishima9622 <rs960222@gmail.com>
2017-02-15 05:32:58 +0900

    テスト

--- listprocess.php
+++ listprocess.php
@@ -1,4 +1,5 @@
 <?php
+require_once("JSON.php");
 /**
  * Created by PhpStorm.
  * User: rj
@@ -15,7 +16,17 @@ $mysqli = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVE
 $lang = $_POST['lang']; //言語情報の取得(jp or en)
 $sort = $_POST['sort']; //ソート条件の格納
 
-$sql = "SELECT spotmaster.spotid,spot_ja.spot_name FROM beaconinfodb.spot_master,beaconinfodb.spot_ja";
+$sql = "SELECT spot_master.spot_id,spot_ja.spot_name FROM beaconti_spot.spot_master,beaconti_spot.spot_ja WHERE spot_ja.spot_id = spot_master.spot_id;";
+$query = mysqli_query($sql);
 
+$spot = array();
+while ($row = mysqli_fetch_object($query)){
+    $spot[]=array(
+        'id' => $row -> spot_id
+        ,'name' => $row -> spot_name
+    );
+}
 
+header('Content-type: text/plain; charset=utf-8');
+echo json_encode($spot);
 ?>
