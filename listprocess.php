d107dc1e5d051cc3a28b41600a425edea0b30a29
kurokirishima9622 <rs960222@gmail.com>
2017-02-15 06:11:17 +0900

    動いたかも

--- listprocess.php
+++ listprocess.php
@@ -1,5 +1,7 @@
 <?php
-require_once("JSON.php");
+ini_set("display_errors", On);
+error_reporting(E_ALL);
+//require_once("JSON.php");
 /**
  * Created by PhpStorm.
  * User: rj
@@ -17,16 +19,17 @@ $lang = $_POST['lang']; //言語情報の取得(jp or en)
 $sort = $_POST['sort']; //ソート条件の格納
 
 $sql = "SELECT spot_master.spot_id,spot_ja.spot_name FROM beaconti_spot.spot_master,beaconti_spot.spot_ja WHERE spot_ja.spot_id = spot_master.spot_id;";
-$query = mysqli_query($sql);
-
-$spot = array();
-while ($row = mysqli_fetch_object($query)){
-    $spot[]=array(
-        'id' => $row -> spot_id
-        ,'name' => $row -> spot_name
-    );
+if($result = $mysqli->query($sql)){
+    echo "rows=" . $result->num_rows;
+    $spot = array();
+    while ($row = $result->fetch_assoc()){
+        $spot[]=array(
+            "id" => $row["spot_id"]
+            ,"name" => $row["spot_name"]
+        );
+    }
+    $result -> close();
 }
-
 header('Content-type: text/plain; charset=utf-8');
 echo json_encode($spot);
 ?>
